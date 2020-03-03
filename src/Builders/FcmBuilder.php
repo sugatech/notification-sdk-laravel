d89a81afdc328d2fa0d119b46dfe9dcbdf25833f
Quang Hung <ngochung.quang.1310@gmail.com>
2020-03-03 15:03:36 +0700

    [#1] Laravel SDK (Refactor fcm builder)

--- src/Builders/FcmBuilder.php
+++ src/Builders/FcmBuilder.php
@@ -7,97 +7,6 @@ use Notification\SDK\Payloads\FcmPayload;
 class FcmBuilder
 {
     /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $title;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $body;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $icon;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $sound;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $channelId;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $badge;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $tag;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $color;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $clickAction;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $bodyLocationKey;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $bodyLocationArgs;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $titleLocationKey;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $titleLocationArgs;
-
-    /**
      * @var int
      */
     protected $notifiableId;
@@ -105,200 +14,12 @@ class FcmBuilder
     /**
      * @var array|null
      */
+    protected $content;
+
+    /**
+     * @var array|null
+     */
     protected $data;
-
-    /**
-     * Indicates notification title. This field is not visible on iOS phones and tablets.
-     * but it is required for android.
-     *
-     * @param string $title
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * Indicates notification body text.
-     *
-     * @param string $body
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setBody($body)
-    {
-        $this->body = $body;
-
-        return $this;
-    }
-
-    /**
-     * Set a channel ID for android API >= 26.
-     *
-     * @param string $channelId
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setChannelId($channelId)
-    {
-        $this->channelId = $channelId;
-
-        return $this;
-    }
-
-    /**
-     * Supported Android
-     * Indicates notification icon. example : Sets value to myicon for drawable resource myicon.
-     *
-     * @param string $icon
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setIcon($icon)
-    {
-        $this->icon = $icon;
-
-        return $this;
-    }
-
-    /**
-     * Indicates a sound to play when the device receives a notification.
-     * Supports default or the filename of a sound resource bundled in the app.
-     *
-     * @param string $sound
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setSound($sound)
-    {
-        $this->sound = $sound;
-
-        return $this;
-    }
-
-    /**
-     * Supported Ios.
-     *
-     * Indicates the badge on the client app home icon.
-     *
-     * @param string $badge
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setBadge($badge)
-    {
-        $this->badge = $badge;
-
-        return $this;
-    }
-
-    /**
-     * Supported Android.
-     *
-     * Indicates whether each notification results in a new entry in the notification drawer on Android.
-     * If not set, each request creates a new notification.
-     * If set, and a notification with the same tag is already being shown, the new notification replaces the existing one in the notification drawer.
-     *
-     * @param string $tag
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setTag($tag)
-    {
-        $this->tag = $tag;
-
-        return $this;
-    }
-
-    /**
-     * Supported Android.
-     *
-     * Indicates color of the icon, expressed in #rrggbb format
-     *
-     * @param string $color
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setColor($color)
-    {
-        $this->color = $color;
-
-        return $this;
-    }
-
-    /**
-     * Indicates the action associated with a user click on the notification.
-     *
-     * @param string $action
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setClickAction($action)
-    {
-        $this->clickAction = $action;
-
-        return $this;
-    }
-
-    /**
-     * Indicates the key to the title string for localization.
-     *
-     * @param string $titleKey
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setTitleLocationKey($titleKey)
-    {
-        $this->titleLocationKey = $titleKey;
-
-        return $this;
-    }
-
-    /**
-     * Indicates the string value to replace format specifiers in the title string for localization.
-     *
-     * @param mixed $titleArgs
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setTitleLocationArgs($titleArgs)
-    {
-        $this->titleLocationArgs = $titleArgs;
-
-        return $this;
-    }
-
-    /**
-     * Indicates the key to the body string for localization.
-     *
-     * @param string $bodyKey
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setBodyLocationKey($bodyKey)
-    {
-        $this->bodyLocationKey = $bodyKey;
-
-        return $this;
-    }
-
-    /**
-     * Indicates the string value to replace format specifiers in the body string for localization.
-     *
-     * @param mixed $bodyArgs
-     *
-     * @return FcmBuilder current instance of the builder
-     */
-    public function setBodyLocationArgs($bodyArgs)
-    {
-        $this->bodyLocationArgs = $bodyArgs;
-
-        return $this;
-    }
 
     /**
      * @param mixed $id
@@ -310,145 +31,24 @@ class FcmBuilder
         return $this;
     }
 
+    /**
+     * @param array $params ['title' => '', 'body' => '', 'sound' => '', 'icon' => '', 'click_action' => '']
+     * @return $this
+     */
+    public function setContent(array $params)
+    {
+        $this->content = $params;
+        return $this;
+    }
 
     /**
      * @param array $data
      * @return $this
      */
-    public function setData($data)
+    public function setData(array $data)
     {
         $this->data = $data;
         return $this;
-    }
-
-    /**
-     * Get title.
-     *
-     * @return null|string
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
-     * Get body.
-     *
-     * @return null|string
-     */
-    public function getBody()
-    {
-        return $this->body;
-    }
-
-    /**
-     * Get channel id for android api >= 26
-     *
-     * @return null|string
-     */
-    public function getChannelId()
-    {
-        return $this->channelId;
-    }
-
-    /**
-     * Get Icon.
-     *
-     * @return null|string
-     */
-    public function getIcon()
-    {
-        return $this->icon;
-    }
-
-    /**
-     * Get Sound.
-     *
-     * @return null|string
-     */
-    public function getSound()
-    {
-        return $this->sound;
-    }
-
-    /**
-     * Get Badge.
-     *
-     * @return null|string
-     */
-    public function getBadge()
-    {
-        return $this->badge;
-    }
-
-    /**
-     * Get Tag.
-     *
-     * @return null|string
-     */
-    public function getTag()
-    {
-        return $this->tag;
-    }
-
-    /**
-     * Get Color.
-     *
-     * @return null|string
-     */
-    public function getColor()
-    {
-        return $this->color;
-    }
-
-    /**
-     * Get ClickAction.
-     *
-     * @return null|string
-     */
-    public function getClickAction()
-    {
-        return $this->clickAction;
-    }
-
-    /**
-     * Get BodyLocationKey.
-     *
-     * @return null|string
-     */
-    public function getBodyLocationKey()
-    {
-        return $this->bodyLocationKey;
-    }
-
-    /**
-     * Get BodyLocationArgs.
-     *
-     * @return null|string|array
-     */
-    public function getBodyLocationArgs()
-    {
-        return $this->bodyLocationArgs;
-    }
-
-    /**
-     * Get TitleLocationKey.
-     *
-     * @return string
-     */
-    public function getTitleLocationKey()
-    {
-        return $this->titleLocationKey;
-    }
-
-    /**
-     * GetTitleLocationArgs.
-     *
-     * @return null|string|array
-     */
-    public function getTitleLocationArgs()
-    {
-        return $this->titleLocationArgs;
     }
 
     /**
@@ -460,7 +60,15 @@ class FcmBuilder
     }
 
     /**
-     * @return array
+     * @return array|null
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return array|null
      */
     public function getData()
     {
--- src/Payloads/FcmPayload.php
+++ src/Payloads/FcmPayload.php
@@ -8,97 +8,6 @@ use Notification\SDK\Builders\FcmBuilder;
 class FcmPayload implements Arrayable
 {
     /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $title;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $body;
-
-    /**
-     * @internal
-     *
-     * @var null/string
-     */
-    protected $channelId;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $icon;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $sound;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $badge;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $tag;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $color;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $clickAction;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $bodyLocationKey;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $bodyLocationArgs;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $titleLocationKey;
-
-    /**
-     * @internal
-     *
-     * @var null|string
-     */
-    protected $titleLocationArgs;
-
-    /**
      * @var int
      */
     protected $notifiableId;
@@ -109,26 +18,20 @@ class FcmPayload implements Arrayable
     protected $data;
 
     /**
+     * @var null|array
+     */
+    protected $content;
+
+    /**
      * FcmBuilder constructor.
      *
      * @param FcmBuilder $builder
      */
     public function __construct(FcmBuilder $builder)
     {
-        $this->title = $builder->getTitle();
-        $this->body = $builder->getBody();
-        $this->channelId = $builder->getChannelId();
-        $this->icon = $builder->getIcon();
-        $this->sound = $builder->getSound();
-        $this->badge = $builder->getBadge();
-        $this->tag = $builder->getTag();
-        $this->color = $builder->getColor();
-        $this->clickAction = $builder->getClickAction();
-        $this->bodyLocationKey = $builder->getBodyLocationKey();
-        $this->bodyLocationArgs = $builder->getBodyLocationArgs();
-        $this->titleLocationKey = $builder->getTitleLocationKey();
-        $this->titleLocationArgs = $builder->getTitleLocationArgs();
+
         $this->notifiableId = $builder->getNotifiableId();
+        $this->content = $builder->getContent();
         $this->data = $builder->getData();
     }
 
@@ -141,21 +44,7 @@ class FcmPayload implements Arrayable
     {
         return [
             'notifiable_id' => $this->notifiableId,
-            'content' => [
-                'title' => $this->title,
-                'body' => $this->body,
-                'android_channel_id' => $this->channelId,
-                'icon' => $this->icon,
-                'sound' => $this->sound,
-                'badge' => $this->badge,
-                'tag' => $this->tag,
-                'color' => $this->color,
-                'click_action' => $this->clickAction,
-                'body_loc_key' => $this->bodyLocationKey,
-                'body_loc_args' => $this->bodyLocationArgs,
-                'title_loc_key' => $this->titleLocationKey,
-                'title_loc_args' => $this->titleLocationArgs,
-            ],
+            'content' => $this->content,
             'data' => $this->data,
         ];
     }
