d5257e0aa8d09de8ed2b1ac405bc6e1806af90d2
Quang Hung <ngochung.quang.1310@gmail.com>
2020-03-03 10:24:24 +0700

    [#1] Laravel SDK (Add notification service provider)

---
+++ src/NotificationClient.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Notification\SDK;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Notification\SDK\Builder\MessageBuilder;
+use Psr\Http\Message\StreamInterface;
+
+class NotificationClient
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $accessToken;
+
+    /**
+     * @var string
+     */
+    private $apiUrl;
+
+    /**
+     * OTPClient constructor.
+     * @param Client $client
+     * @param string $apiUrl
+     * @param string $accessToken
+     */
+    public function __construct($client, $apiUrl, $accessToken)
+    {
+        $this->client = $client;
+        $this->accessToken = $accessToken;
+        $this->apiUrl = $apiUrl;
+    }
+
+    /**
+     * @param MessageBuilder $message
+     * @return StreamInterface
+     */
+    public function send($message)
+    {
+        $response = $this->client->post($this->apiUrl.'/api/client/v1/message/send', [
+            RequestOptions::HEADERS => [
+                'Content-Type' => 'application/json',
+                'Authorization' => $this->accessToken,
+            ],
+            RequestOptions::JSON => [
+                'channels' => $message,
+            ]
+        ]);
+
+        return \GuzzleHttp\json_decode($response->getBody(), true);
+    }
+}
