f26eb2e35441a3fbc0af8db5bd1347e2f474319d
Quang Hung <ngochung.quang.1310@gmail.com>
2020-03-03 15:32:38 +0700

    [#1] Laravel SDK (Add fcm topic builder)

---
+++ src/Builders/FcmTopicBuilder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Notification\SDK\Builders;
+
+use Notification\SDK\Payloads\FcmTopicPayload;
+
+class FcmTopicBuilder
+{
+    /**
+     * @var string
+     */
+    protected $topic;
+
+    /**
+     * @var array|null
+     */
+    protected $content;
+
+    /**
+     * @var array|null
+     */
+    protected $data;
+
+    /**
+     * @param string $topic
+     * @return $this
+     */
+    public function setTopic($topic)
+    {
+        $this->topic = $topic;
+        return $this;
+    }
+
+    /**
+     * @param array $params ['title' => '', 'body' => '', 'sound' => '', 'icon' => '', 'click_action' => '']
+     * @return $this
+     */
+    public function setContent(array $params)
+    {
+        $this->content = $params;
+        return $this;
+    }
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTopic()
+    {
+        return $this->topic;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Build an FcmTopicBuilder.
+     *
+     * @return FcmTopicPayload
+     */
+    public function build()
+    {
+        return new FcmTopicPayload($this);
+    }
+}
