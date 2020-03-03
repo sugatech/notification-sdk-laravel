826ac27724c8a75efa0501a21c414f5908c649fc
Quang Hung <ngochung.quang.1310@gmail.com>
2020-03-03 16:16:01 +0700

    [#1] Laravel SDK (Update message builder)

--- src/Builders/MessageBuilder.php
+++ src/Builders/MessageBuilder.php
@@ -2,8 +2,13 @@
 
 namespace Notification\SDK\Builders;
 
+use Notification\SDK\Payloads\DatabasePayload;
 use Notification\SDK\Payloads\FcmPayload;
+use Notification\SDK\Payloads\FcmTokenPayload;
+use Notification\SDK\Payloads\FcmTopicPayload;
+use Notification\SDK\Payloads\MailPayload;
 use Notification\SDK\Payloads\MessagePayload;
+use Notification\SDK\Payloads\SmsPayload;
 
 class MessageBuilder
 {
@@ -11,6 +16,31 @@ class MessageBuilder
      * @var FcmPayload
      */
     private $fcmPayload;
+
+    /**
+     * @var DatabasePayload
+     */
+    private $databasePayload;
+
+    /**
+     * @var MailPayload
+     */
+    private $mailPayload;
+
+    /**
+     * @var SmsPayload
+     */
+    private $smsPayload;
+
+    /**
+     * @var FcmTokenPayload
+     */
+    private $fcmTokenPayload;
+
+    /**
+     * @var FcmTopicPayload
+     */
+    private $fcmTopicPayload;
 
     /**
      * @param FcmPayload $fcmPayload
@@ -28,6 +58,97 @@ class MessageBuilder
     public function getFcmPayload()
     {
         return $this->fcmPayload;
+    }
+
+
+    /**
+     * @return DatabasePayload
+     */
+    public function getDatabasePayload()
+    {
+        return $this->databasePayload;
+    }
+
+    /**
+     * @param DatabasePayload $databasePayload
+     * @return MessageBuilder
+     */
+    public function setDatabasePayload($databasePayload)
+    {
+        $this->databasePayload = $databasePayload;
+        return $this;
+    }
+
+    /**
+     * @return MailPayload
+     */
+    public function getMailPayload()
+    {
+        return $this->mailPayload;
+    }
+
+    /**
+     * @param MailPayload $mailPayload
+     * @return MessageBuilder
+     */
+    public function setMailPayload($mailPayload)
+    {
+        $this->mailPayload = $mailPayload;
+        return $this;
+    }
+
+    /**
+     * @return SmsPayload
+     */
+    public function getSmsPayload()
+    {
+        return $this->smsPayload;
+    }
+
+    /**
+     * @param SmsPayload $smsPayload
+     * @return MessageBuilder
+     */
+    public function setSmsPayload($smsPayload)
+    {
+        $this->smsPayload = $smsPayload;
+        return $this;
+    }
+
+    /**
+     * @return FcmTokenPayload
+     */
+    public function getFcmTokenPayload()
+    {
+        return $this->fcmTokenPayload;
+    }
+
+    /**
+     * @param FcmTokenPayload $fcmTokenPayload
+     * @return MessageBuilder
+     */
+    public function setFcmTokenPayload($fcmTokenPayload)
+    {
+        $this->fcmTokenPayload = $fcmTokenPayload;
+        return $this;
+    }
+
+    /**
+     * @return FcmTopicPayload
+     */
+    public function getFcmTopicPayload()
+    {
+        return $this->fcmTopicPayload;
+    }
+
+    /**
+     * @param FcmTopicPayload $fcmTopicPayload
+     * @return MessageBuilder
+     */
+    public function setFcmTopicPayload($fcmTopicPayload)
+    {
+        $this->fcmTopicPayload = $fcmTopicPayload;
+        return $this;
     }
 
     /**
--- src/Payloads/MessagePayload.php
+++ src/Payloads/MessagePayload.php
@@ -3,15 +3,39 @@
 namespace Notification\SDK\Payloads;
 
 use Illuminate\Contracts\Support\Arrayable;
-use Notification\SDK\Builders\FcmBuilder;
 use Notification\SDK\Builders\MessageBuilder;
 
 class MessagePayload implements Arrayable
 {
     /**
-     * @var FcmBuilder
+     * @var FcmPayload
      */
-    private $fcmBuilder;
+    private $fcmPayload;
+
+    /**
+     * @var DatabasePayload
+     */
+    private $databasePayload;
+
+    /**
+     * @var MailPayload
+     */
+    private $mailPayload;
+
+    /**
+     * @var SmsPayload
+     */
+    private $smsPayload;
+
+    /**
+     * @var FcmTokenPayload
+     */
+    private $fcmTokenPayload;
+
+    /**
+     * @var FcmTopicPayload
+     */
+    private $fcmTopicPayload;
 
     /**
      * MessagePayload constructor.
@@ -19,7 +43,12 @@ class MessagePayload implements Arrayable
      */
     public function __construct($message)
     {
-        $this->fcmBuilder = $message->getFcmPayload();
+        $this->fcmPayload = $message->getFcmPayload();
+        $this->databasePayload = $message->getDatabasePayload();
+        $this->mailPayload = $message->getMailPayload();
+        $this->smsPayload = $message->getSmsPayload();
+        $this->fcmTokenPayload = $message->getFcmTokenPayload();
+        $this->fcmTopicPayload = $message->getFcmTopicPayload();
     }
 
     /**
@@ -28,7 +57,12 @@ class MessagePayload implements Arrayable
     public function toArray()
     {
         $message = [
-            'fcm' => $this->fcmBuilder,
+            'fcm' => $this->fcmPayload,
+            'database' => $this->databasePayload,
+            'mail' => $this->mailPayload,
+            'sms' => $this->smsPayload,
+            'fcm_token' => $this->fcmTokenPayload,
+            'fcm_topic' => $this->fcmTopicPayload,
         ];
 
         $message = array_filter($message, function($value) {
