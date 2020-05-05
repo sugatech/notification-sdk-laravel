dca91f683ec6b55bc235b3a81c81c1dcc7ce0e8b
Quang Hung <ngochung.quang.1310@gmail.com>
2020-05-05 17:19:56 +0700

    [#12] Use oauth2-client-credentials-laravel lib (Add sugatech/oauth2-client-credentials-laravel package)

--- config/notification.php
+++ config/notification.php
@@ -2,7 +2,10 @@
 
 return [
     'api_url' => env('NOTIFICATION_API_URL'),
-    'access_token' => env('NOTIFICATION_ACCESS_TOKEN'),
-
+    'oauth' => [
+        'url' => env('NOTIFICATION_OAUTH_URL', env('NOTIFICATION_API_URL').'/oauth/token'),
+        'client_id' => env('NOTIFICATION_OAUTH_CLIENT_ID'),
+        'client_secret' => env('NOTIFICATION_OAUTH_CLIENT_SECRET'),
+    ],
     'channel_background' => env('NOTIFICATION_CHANNEL_BACKGROUND', false),
 ];
